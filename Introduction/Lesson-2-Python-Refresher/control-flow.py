c1e8866fbf2414aef85ad8ad567056aa1e7aeea5
Nitesh Sharma <nitesh.grep@gmail.com>
2019-07-11 10:23:59 +0530

    Python Introduction - Control Flow
    Adding for and while loop

---
+++ Introduction/Lesson-2-Python-Refresher/control-flow.py
@@ -0,0 +1,23 @@
+my_list = [1,2,3,4]
+
+for i in my_list:
+  print(i)
+
+for i, item in enumerate(my_list):
+  print( 'The value of item is:' + str(item) + 'value' + str(i));
+
+i=0;
+while(i<10):
+  print(i);
+  i += 1;
+
+my_dict = {
+  'a': 'Nitesh',
+  'b': 'Akhil',
+  'c': 'Lin'
+}
+
+for key in my_dict:
+  print(key + ',' + my_dict[key])
+
+  
